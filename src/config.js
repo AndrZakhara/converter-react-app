1a4e0e83332c24d68abafb7f4ae3ca6103f445b4
Andrii Zakharchenko <art.pruttov@gmail.com>
2019-03-06 12:01:05 +0200

    boilerplate, * add react-redux-firebase, * add redux-form

---
+++ src/config.js
@@ -0,0 +1,11 @@
+export const FIREBASE_CONFIG = {
+  // Change this to your firebase configuration! (Add Firebase to your web app)
+    FIREBASE_CONFIG: {
+      apiKey: "AIzaSyDyizHye2-mIQo2obX3-F0OczVcepFvCAg",
+      authDomain: "currency-converter-ab797.firebaseapp.com",
+      databaseURL: "https://currency-converter-ab797.firebaseio.com",
+      projectId: "currency-converter-ab797",
+      storageBucket: "currency-converter-ab797.appspot.com",
+      messagingSenderId: "756428117452"
+    },
+};
