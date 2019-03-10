a06eeee27b703fe84c99e39b43a817e4141fed74
Bodyan322 <bogdan3206@gmail.com>
2019-03-10 16:03:08 +0200

    feature/home, add material ui components to hone page

--- src/components/Home/index.js
+++ src/components/Home/index.js
@@ -1,11 +1,25 @@
+// import React from 'react';
+
+// const Home = () => {
+//   return (
+//     <div className = 'content'>
+//       <h2>Main page</h2>
+//     </div>
+//   );
+// };
+
+// export default Home;
 import React from 'react';
-
-const Home = () => {
-  return (
-    <div className = 'content'>
-      <h2>Main page</h2>
-    </div>
-  );
-};
-
-export default Home;
+import PropTypes from 'prop-types';
+import { withStyles } from '@material-ui/core/styles';
+import {
+  Table,
+  TableBody,
+  TableHead,
+  TablePagination,
+  TableRow,
+  TableSortLabel,
+  Paper,
+  Tooltip,
+  TableCell
+} from '@material-ui/core';
