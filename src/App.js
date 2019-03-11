50708a11d46c9cb35aaf929e19932c843a467729
ZelikSV <zeliksv@gmail.com>
2019-03-11 21:49:44 +0200

    feature/converter, fix eslinter errors

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
-import React, { Component, Fragment }  from 'react';
+import React, { Component, Fragment } from 'react';
+import { Switch, Route } from 'react-router-dom';
 import { Header, Home, LoginForm, News, Profile } from './components';
 import Converter from './pages/Converter';
-import { Switch, Route } from 'react-router-dom';
 import Footer from './components/Footer';
 
 class App extends Component {
@@ -10,15 +10,14 @@ class App extends Component {
       <Fragment>
         <Header />
         <Switch>
-          <Route exact path = '/' 
-          component = {Home} />
-          <Route path = '/login' component = {LoginForm} />
-          <Route path = '/news' component = {News} />
-          <Route path = '/profile' component = {Profile} />
-          <Route path = '/converter' component = {Converter} />
+          <Route exact path="/" component={Home} />
+          <Route path="/login" component={LoginForm} />
+          <Route path="/news" component={News} />
+          <Route path="/profile" component={Profile} />
+          <Route path="/converter" component={Converter} />
         </Switch>
         <Footer />
-      </Fragment>      
+      </Fragment>
     );
   }
 }
--- src/sagas/currencySaga/index.js
+++ src/sagas/currencySaga/index.js
@@ -1,14 +1,14 @@
-import { put, call, takeEvery } from "redux-saga/effects";
-import axios from "axios";
+import { put, call, takeEvery } from 'redux-saga/effects';
+import axios from 'axios';
 import { ADD_CURRENCY, addCurrencyAsync } from '../../actions/currencyAction';
 
 function* getCurrencies() {
-  const currenciesList = yield call(() => {
-  return axios.get("https://api.privatbank.ua/p24api/pubinfo?json&exchange&coursid=5")
-        .then(item => item.data);
-  });
+  const currenciesList = yield call(() =>
+    axios
+      .get('https://api.privatbank.ua/p24api/pubinfo?json&exchange&coursid=5')
+      .then(item => item.data),
+  );
   yield put(addCurrencyAsync(currenciesList));
-
 }
 
 export default function* watchGetAllCurrencies() {
