21e764f97188c14caa97f96f449b0ed8bfe61594
Andrii Zakharchenko <art.pruttov@gmail.com>
2019-03-13 21:37:10 +0200

    fix/admi_actions, fix actions syntaxis and links

--- src/actions/types.js
+++ src/actions/types.js
@@ -8,3 +8,8 @@ export const SAVE_PROFILE = 'EDIT_PROFILE';
 export const SIGNUP = 'SIGNUP';
 export const SIGNUP_SUCCESS = 'SIGNUP_SUCCESS';
 export const SIGNUP_ERROR = 'SIGNUP_ERROR';
+
+export const GET_ALL_USERS = 'GET_ALL_USERS';
+export const GET_USER_DATA = 'GET_USER_DATA';
+export const RECIVE_ALL_USERS = 'RECIVE_ALL_USERS';
+export const SET_SELECTED_USER = 'SET_SELECTED_USER';
