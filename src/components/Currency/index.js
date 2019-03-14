ac912c29c4c982c2f4499d02a7c83baadee14b95
ZelikSV <zeliksv@gmail.com>
2019-03-14 23:58:12 +0200

    fix/Converter, replace function convert to helpers

--- src/components/Currency/index.js
+++ src/components/Currency/index.js
@@ -8,51 +8,44 @@ import { withStyles } from '@material-ui/core/styles';
 
 import { feeConvert } from '../../mocks/db';
 import { styles } from './style';
-import { convertToUa, convertFromUa, countTax } from '../../helpers/converter.helper';
+import { buyCurrency } from '../../helpers/converter.helper';
 
 class Currency extends Component {
   componentDidMount() {
     this.props.addCurrency();
-  }
+    this.props.countCurrency();
+  };
 
   _changeCurrencies = () => {
     const { currenciesCount, change } = this.props;
-    const { currencyBuy, currencySell, amountSell, amountBuy } = currenciesCount.values;
+    const {
+      currencyBuy,
+      currencySell,
+      amountSell,
+      amountBuy,
+    } = currenciesCount.values;
     change('currencyBuy', currencySell);
-    change('currencySell',currencyBuy);
+    change('currencySell', currencyBuy);
     change('amountBuy', amountSell);
     change('amountSell', amountBuy);
   };
 
-
   _buyCurrency = () => {
-    this.props.countCurrency();
-    const { currencies, currenciesCount, change } = this.props;
+    const { currenciesCount, change, currencies } = this.props;
     const { currencyBuy, currencySell, amountSell, fee } = currenciesCount.values;
-
-    const indexSellCurrency = currencies.findIndex(item => item.ccy === currencySell);
-    const indexBuyCurrency = currencies.findIndex(item => item.ccy === currencyBuy);
-
-    const firstConvert = convertToUa( amountSell, currencies[indexSellCurrency].buy);
-    const secondConvert = convertFromUa(firstConvert, currencies[indexBuyCurrency].sale);
-    const countWithTax = secondConvert - countTax(secondConvert, fee);
-
-    change('amountBuy', Math.trunc(countWithTax * 100) / 100);
+    buyCurrency(currencies, currencySell, currencyBuy, amountSell, fee, change);
   };
 
   render() {
     const { currencies, classes, handleSubmit } = this.props;
-    const { _changeCurrencies, _buyCurrency } = this;
-
-    const selectsOptions = oppositeCurrency =>(
+    const selectsOptions = oppositeCurrency =>
       currencies
         .filter(item => item.ccy !== oppositeCurrency)
         .map(item => (
           <option value={item.ccy} key={item.ccy}>
             {item.ccy}
           </option>
-        )));
-
+        ));
     const feeSelectsOption = () =>
       feeConvert.map(item => (
         <option value={item} key={item}>
@@ -62,7 +55,7 @@ class Currency extends Component {
 
     return (
       <form
-      name="currencyForm"
+        name="currencyForm"
         className={classes.appContent}
         onSubmit={handleSubmit}>
         <div className={classes.converterTitle}>
@@ -72,43 +65,42 @@ class Currency extends Component {
           <FormControl className={classes.formControl}>
             <p className={classes.text}>give back</p>
             <Field
-             className={classes.select}
+              className={classes.select}
               name="currencySell"
               component="select"
-              onChange={ _buyCurrency }
-              >
-            {selectsOptions()}
-              </Field>
+              onChange={this._buyCurrency}>
+              {selectsOptions()}
+            </Field>
           </FormControl>
           <Button
             variant="contained"
             color="primary"
-            onClick={_changeCurrencies}>
+            onClick={this._changeCurrencies}>
             &#8660;
           </Button>
           <FormControl className={classes.formControl}>
             <p className={classes.text}>we get</p>
             <Field
-            className={classes.select}
+              className={classes.select}
               name="currencyBuy"
               component="select"
-              onChange={ _buyCurrency }>
-            {selectsOptions()}
+              onChange={this._buyCurrency}>
+              {selectsOptions()}
             </Field>
           </FormControl>
         </div>
         <div className={classes.currencyLine}>
           <Field
-          className={classes.inputAmount}
+            className={classes.inputAmount}
             name="amountSell"
             component="input"
             type="number"
             label="How much to exchange"
             min="0"
-            onChange={ _buyCurrency }
+            onChange={this._buyCurrency}
           />
           <Field
-          className={classes.inputAmount}
+            className={classes.inputAmount}
             name="amountBuy"
             component="input"
             type="number"
@@ -120,11 +112,11 @@ class Currency extends Component {
           <FormControl className={classes.feeWrapper}>
             <p className={classes.text}>Fee </p>
             <Field
-            className={classes.feeSelect}
+              className={classes.feeSelect}
               name="fee"
               component="select"
-              onChange={ _buyCurrency }>
-            {feeSelectsOption()}
+              onChange={this._buyCurrency}>
+              {feeSelectsOption()}
             </Field>
             <p className={classes.text}> %</p>
           </FormControl>
@@ -146,14 +138,16 @@ Currency.propTypes = {
 };
 
 export default compose(
-  reduxForm({ form: 'currencyForm',
-  enableReinitialize: true, 
-  initialValues: {
-    currencyBuy: 'EUR',
-    currencySell: 'USD',
-    amountSell: 0,
-    amountBuy: 0,
-    fee: 2,
-  } }),
+  reduxForm({
+    form: 'currencyForm',
+    enableReinitialize: true,
+    initialValues: {
+      currencyBuy: 'EUR',
+      currencySell: 'USD',
+      amountSell: 0,
+      amountBuy: 0,
+      fee: 2,
+    },
+  }),
   withStyles(styles),
 )(Currency);
--- src/helpers/converter.helper.js
+++ src/helpers/converter.helper.js
@@ -1,17 +1,38 @@
-export const convertToUa = (from, to) => {
+const convertToUa = (from, to) => {
   const result = from * to;
 
   return result;
 };
 
-export const convertFromUa = (from, to) => {
+const convertFromUa = (from, to) => {
   const res = from / to;
 
   return res;
 };
 
-export const countTax = (sum, pr) => {
+const countTax = (sum, pr) => {
   const res = (sum * pr) / 100;
 
   return res;
+};
+
+export const buyCurrency = (array, CurrencyNameFrom, CurrencyNameTo, AmountSell, tax, cb) => {
+
+  const indexSellCurrency = array.findIndex(
+    item => item.ccy === CurrencyNameFrom,
+  );
+  const indexBuyCurrency = array.findIndex(
+    item => item.ccy === CurrencyNameTo,
+  );
+
+  const firstConvert = convertToUa(
+    AmountSell,
+    array[indexSellCurrency].buy,
+  );
+  const secondConvert = convertFromUa(
+    firstConvert,
+    array[indexBuyCurrency].sale,
+  );
+  const countWithTax = secondConvert - countTax(secondConvert, tax);
+  cb('amountBuy', Math.trunc(countWithTax * 100) / 100);
 };
