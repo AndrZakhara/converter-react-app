7741728b6baaa3056014968b2d0fac5339bd719b
mneokvsenorm <mneokvsenorm@gmail.com>
2019-03-14 16:58:35 +0200

    fix/profile, move request from saga to api

---
+++ src/api/getProfile/index.js
@@ -0,0 +1,4 @@
+import axios from 'axios';
+
+// eslint-disable-next-line import/prefer-default-export
+export const getProfile = () => axios.get('https://api.myjson.com/bins/r36wy');
--- src/sagas/userSaga/index.js
+++ src/sagas/userSaga/index.js
@@ -1,13 +1,13 @@
 /* eslint-disable import/prefer-default-export */
 import { call, put, take } from 'redux-saga/effects';
-import axios from 'axios';
+import { getProfile } from '../../api/getProfile';
 import { FETCH_USER } from '../../actions/types';
 import { fetchUserSuccess, serverError } from '../../actions';
 
 export function* fetchUserSaga() {
   try {
     yield take(FETCH_USER);
-    const { data } = yield call(axios.get, 'https://api.myjson.com/bins/r36wy');
+    const { data } = yield call(getProfile);
     yield put(fetchUserSuccess(data.user));
   } catch (e) {
     yield put(serverError());
