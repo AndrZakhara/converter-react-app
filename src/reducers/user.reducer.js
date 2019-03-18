4214d36508bcb68547899463324a0877b7e02246
Bodyan322 <bogdan3206@gmail.com>
2019-03-18 17:52:43 +0200

    feature/home, rewrite reducer for homePage and for profilePage

--- src/reducers/user.reducer.js
+++ src/reducers/user.reducer.js
@@ -1,42 +1,24 @@
+import combineEvents from 'utils/combineEvents';
 import {
   FETCH_USER,
   FETCH_USER_SUCCESS,
   SAVE_PROFILE,
   FETCH_USER_CURRENCY_DIALS,
-} from '../actions/types';
+} from 'actions/types';
 
 const initialState = {
   profile: null,
   error: null,
 };
 
-export default (state = initialState, { type, payload }) => {
-  switch (type) {
-    case FETCH_USER:
-      return {
-        ...state,
-        error: null,
-      };
-    case FETCH_USER_SUCCESS:
-      return {
-        ...state,
-        profile: payload,
-      };
-    case SAVE_PROFILE:
-      return {
-        ...state,
-        profile: payload,
-      };
-    case FETCH_USER_CURRENCY_DIALS:
-      return {
-        ...state,
-        userDials: payload,
-      };
-
-    default:
-      return {
-        ...state,
-      };
-  }
-};
-
+export default combineEvents(
+  {
+    [FETCH_USER]: state => ({ error: null }),
+    [FETCH_USER_SUCCESS]: (state, { payload }) => ({ profile: payload }),
+    [SAVE_PROFILE]: (state, { payload }) => ({ profile: payload }),
+    [FETCH_USER_CURRENCY_DIALS]: (state, { payload }) => ({
+      userDials: payload,
+    }),
+  },
+  initialState,
+);
