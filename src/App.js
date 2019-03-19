a146ce37661ca8e4db7d1d6e45839c97c2c48737
AlexYalovoy <YalovyyAlex@gmail.com>
2019-03-19 12:13:41 +0200

    feature/Auth add private routes

--- src/App.js
+++ src/App.js
@@ -1,23 +1,22 @@
 /* eslint-disable react/prefer-stateless-function */
 import React, { Fragment } from 'react';
+import PrivateRoute from 'pages/PrivateRoute';
 import { connect } from 'react-redux';
 import { Switch, Route, Redirect } from 'react-router-dom';
-import { Header, Home, LoginForm, Footer } from './components';
+import { Header, Home, Footer } from './components';
 import { Admin, Converter, Profile, SignUp, SignIn } from './pages';
 
 const App = ({ isLoggedIn }) => (
   <Fragment>
     <Header />
     <Switch>
-      <Route exact path="/" component={Home} />
-      <Route path="/login" component={LoginForm} />
-      <Route path="/admin-panel" component={Admin} />
-      <Route path="/profile" component={Profile} />
-      <Route path="/converter" component={Converter} />
+      <PrivateRoute exact path="/" component={Home} />
+      <PrivateRoute path="/admin-panel" component={Admin} />
+      <PrivateRoute path="/profile" component={Profile} />
+      <PrivateRoute path="/converter" component={Converter} />
       <Route path="/sign-up" component={SignUp} />
       <Route path="/sign-in" component={SignIn} />
     </Switch>
-    {!isLoggedIn && <Redirect to="/sign-in" />}
     <Footer />
   </Fragment>
 );
