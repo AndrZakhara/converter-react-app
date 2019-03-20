0c3c204b60665b5fc22a4a6d6f6f6e29c051fc1e
AlexYalovoy <YalovyyAlex@gmail.com>
2019-03-20 15:46:43 +0200

    feature/Auth delete .env.development from branch

--- src/api/database/index.js
+++ src/api/database/index.js
@@ -4,12 +4,3 @@ export const createUserInDB = (uid, ava, email, firstName, lastName, phone) =>
   fb.doCreateUserInDatabase(uid, ava, email, firstName, lastName, phone);
 
 export const getUserfromDB = uid => fb.getUserFromDatabase(uid);
-
-createUserInDB(
-  '2x6QppJCggXYsN1zAZg78DaWXkj2',
-  '',
-  'alexyalovoy@mail.ru',
-  'alex',
-  'yalovoy',
-  '3806311111',
-);
