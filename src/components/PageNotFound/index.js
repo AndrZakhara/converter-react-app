fdbd0117cb4b98a42b8838e2e2597432b4500914
mneokvsenorm <mneokvsenorm@gmail.com>
2019-03-26 18:40:03 +0200

    feature/pageNotFound, move gif to assets

--- src/components/PageNotFound/index.js
+++ src/components/PageNotFound/index.js
@@ -2,14 +2,12 @@ import React from 'react';
 import { Card, withStyles } from '@material-ui/core';
 import CardMedia from '@material-ui/core/CardMedia';
 
+import image from 'assets/images/404_image.gif';
 import styles from './styles';
 
 const PageNotFound = ({ classes }) => (
   <Card className={classes.container}>
-    <CardMedia
-      className={classes.media}
-      image="http://www.reactiongifs.us/wp-content/uploads/2015/04/nothing_to_see_here_naked_gun.gif"
-    />
+    <CardMedia className={classes.media} image={image} />
     <div className={classes.mediaTitle}>
       <h2 className={classes.mediaTitleMain}>404</h2>
       <p>Ooops, the page you are looking for does not exist.</p>
