29ee32d445c399c85670d19f30416f9c96b34ca2
AlexYalovoy <YalovyyAlex@gmail.com>
2019-03-26 23:47:19 +0200

    fix/Auth fix merge errors

--- src/routes/index.js
+++ src/routes/index.js
@@ -1,17 +1,28 @@
 import React from 'react';
 import { Switch, Route } from 'react-router-dom';
 
-import { Admin, Converter, Profile, SignUp, SignIn, Home } from 'pages';
+import {
+  Admin,
+  Converter,
+  Profile,
+  SignUp,
+  SignIn,
+  Home,
+  WeatherWrap,
+} from 'pages';
+import { PageNotFound } from 'components';
 import AdminRoute from './admin.route';
 import PrivateRoute from './private.route';
 
-export default (
+export default () => (
   <Switch>
     <PrivateRoute exact path="/" component={Home} />
     <AdminRoute path="/admin-panel" component={Admin} />
     <PrivateRoute path="/profile" component={Profile} />
     <PrivateRoute path="/converter" component={Converter} />
+    <PrivateRoute path="/weather" component={WeatherWrap} />
     <Route path="/sign-up" component={SignUp} />
     <Route path="/sign-in" component={SignIn} />
+    <Route path="*" component={PageNotFound} />
   </Switch>
 );
