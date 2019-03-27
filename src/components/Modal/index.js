ecb308f00c2c6914adbd4d80ebcce0395b6591df
Andrii Zakharchenko <art.pruttov@gmail.com>
2019-03-27 14:35:29 +0200

    feature/modal_window, fix destructor in modal props

--- src/components/Modal/index.js
+++ src/components/Modal/index.js
@@ -8,31 +8,24 @@ import Button from '@material-ui/core/Button';
 
 import styles from './style';
 
-const ModalWindow = props => {
-  const { isOpen, closeModal, classes, modalMessage } = props;
-
-  return (
-    <Modal className={classes.modalWindow} isOpen={isOpen}>
-      <Paper className={classes.modalContentWrapper}>
-        <span className={classes.mes}>{modalMessage}</span>
-        <div className={classes.buttonModalWrapper}>
-          <Button
-            className={classes.button}
-            onClick={closeModal}
-            variant="contained"
-            color="primary">
-            Cancel
-          </Button>
-          <Button
-            className={classes.button}
-            variant="contained"
-            color="primary">
-            Apply
-          </Button>
-        </div>
-      </Paper>
-    </Modal>
-  );
-};
+const ModalWindow = ({ isOpen, closeModal, classes, modalMessage }) => (
+  <Modal className={classes.modalWindow} isOpen={isOpen}>
+    <Paper className={classes.modalContentWrapper}>
+      <span className={classes.mes}>{modalMessage}</span>
+      <div className={classes.buttonModalWrapper}>
+        <Button
+          className={classes.button}
+          onClick={closeModal}
+          variant="contained"
+          color="primary">
+          Cancel
+        </Button>
+        <Button className={classes.button} variant="contained" color="primary">
+          Apply
+        </Button>
+      </div>
+    </Paper>
+  </Modal>
+);
 
 export default compose(withStyles(styles))(ModalWindow);
