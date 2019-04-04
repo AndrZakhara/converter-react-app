ea0e81b6cea2efb280a8d6a0783ba75552b5744c
Bodyan322 <bogdan3206@gmail.com>
2019-04-04 15:01:04 +0300

    styledComponent/HomePage, add primary color

--- src/components/EnhancedTable/EnhancedTable.js
+++ src/components/EnhancedTable/EnhancedTable.js
@@ -19,12 +19,19 @@ const StyledPaper = styled(Paper)`
 const TableTitle = styled.h1`
   background: #3f51b5;
   color: #fff;
+  background: ${props =>
+    props.primary
+      ? '#3f51b5'
+      : 'linear-gradient(45deg, #fe6b8b 30%, #ff8e53 90%)'};
   border-radius: 4px;
   border-bottom-left-radius: 0;
   border-bottom-right-radius: 0;
   margin: 0;
   padding: 10px 0;
-  border: 1px solid #3f51b5;
+  border: ${props =>
+    props.primary
+      ? '1px solid #3f51b5'
+      : 'linear-gradient(45deg, #fe6b8b 30%, #ff8e53 90%)'};
   box-shadow: 0px 1px 5px 0px rgba(0, 0, 0, 0.2),
     0px 2px 2px 0px rgba(0, 0, 0, 0.14), 0px 3px 1px -2px rgba(0, 0, 0, 0.12);
   text-transform: capitalize;
@@ -83,7 +90,7 @@ class EnhancedTable extends Component {
         rowsPerPage - Math.min(rowsPerPage, data.length - page * rowsPerPage);
       return (
         <StyledPaper>
-          <TableTitle>History of Converting values</TableTitle>
+          <TableTitle primary>History of Converting values</TableTitle>
           <StyledTableWrapper>
             <StyledTable aria-labelledby="tableTitle">
               <EnhancedTableHead
