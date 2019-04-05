e6709bf31149ccf1416010d47bd2035bd48ad2d2
Bodyan322 <bogdan3206@gmail.com>
2019-04-05 15:21:53 +0300

    styledComponent/Header, move themeProvider to root component

--- src/App.js
+++ src/App.js
@@ -1,13 +1,17 @@
 import React, { Fragment } from 'react';
 import { Header, Footer } from 'components';
 import RootRouter from 'routes';
+import { ThemeProvider, withTheme } from 'styled-components';
+import theme from './styles';
 
 const App = () => (
   <Fragment>
-    <Header />
+    <ThemeProvider theme={theme}>
+      <Header />
+    </ThemeProvider>
     <RootRouter />
     <Footer />
   </Fragment>
 );
 
-export default App;
+export default withTheme(App);
--- src/components/Header/index.js
+++ src/components/Header/index.js
@@ -5,7 +5,6 @@ import Person from '@material-ui/icons/Person';
 import ExitToApp from '@material-ui/icons/ExitToApp';
 import { signOut } from 'actions';
 import { ADMIN } from 'constants/roles';
-import { ThemeProvider, withTheme } from 'styled-components';
 import {
   StyledAppBar,
   StyledToolbar,
@@ -14,74 +13,71 @@ import {
   ButtonLogo,
   Logo,
 } from './styles';
-import theme from '../../styles';
 
 const Header = ({ isLoggedIn, role, onLogOut }) => (
   <>
-    <ThemeProvider theme={theme}>
-      <StyledAppBar position="static">
-        <StyledToolbar>
-          {isLoggedIn ? (
-            <Brand>
-              <StyledLink to="/">
-                <ButtonLogo>
-                  <Logo variant="title">LOGO</Logo>
-                </ButtonLogo>
-              </StyledLink>
-            </Brand>
-          ) : (
-            <Brand>
-              <Logo variant="title">LOGO</Logo>
-            </Brand>
-          )}
-          {isLoggedIn ? (
-            <Fragment>
-              {role === ADMIN ? (
-                <Fragment>
-                  <StyledLink to="/admin-panel">
-                    <ButtonLogo>
-                      <span>Admin Panel</span>
-                    </ButtonLogo>
-                  </StyledLink>
-                </Fragment>
-              ) : null}
-              <StyledLink to="/converter">
-                <ButtonLogo>
-                  <span>Converter</span>
-                </ButtonLogo>
-              </StyledLink>
-              <StyledLink to="/weather">
-                <ButtonLogo>
-                  <span>Weather</span>
-                </ButtonLogo>
-              </StyledLink>
-              <StyledLink to="/profile">
-                <ButtonLogo>
-                  <Person />
-                </ButtonLogo>
-              </StyledLink>
-              <ButtonLogo onClick={onLogOut}>
-                <ExitToApp />
-                <span>Logout</span>
+    <StyledAppBar position="static">
+      <StyledToolbar>
+        {isLoggedIn ? (
+          <Brand>
+            <StyledLink to="/">
+              <ButtonLogo>
+                <Logo variant="title">LOGO</Logo>
               </ButtonLogo>
-            </Fragment>
-          ) : (
-            <Fragment>
-              <StyledLink to="/sign-in">
-                <ButtonLogo>
-                  <span>Sign In</span>
-                </ButtonLogo>
-              </StyledLink>
-              <StyledLink to="/sign-up">
-                <ButtonLogo>
-                  <span>Sign Up</span>
-                </ButtonLogo>
-              </StyledLink>
-            </Fragment>
-          )}
-        </StyledToolbar>
-      </StyledAppBar>
-    </ThemeProvider>
+            </StyledLink>
+          </Brand>
+        ) : (
+          <Brand>
+            <Logo variant="title">LOGO</Logo>
+          </Brand>
+        )}
+        {isLoggedIn ? (
+          <Fragment>
+            {role === ADMIN ? (
+              <Fragment>
+                <StyledLink to="/admin-panel">
+                  <ButtonLogo>
+                    <span>Admin Panel</span>
+                  </ButtonLogo>
+                </StyledLink>
+              </Fragment>
+            ) : null}
+            <StyledLink to="/converter">
+              <ButtonLogo>
+                <span>Converter</span>
+              </ButtonLogo>
+            </StyledLink>
+            <StyledLink to="/weather">
+              <ButtonLogo>
+                <span>Weather</span>
+              </ButtonLogo>
+            </StyledLink>
+            <StyledLink to="/profile">
+              <ButtonLogo>
+                <Person />
+              </ButtonLogo>
+            </StyledLink>
+            <ButtonLogo onClick={onLogOut}>
+              <ExitToApp />
+              <span>Logout</span>
+            </ButtonLogo>
+          </Fragment>
+        ) : (
+          <Fragment>
+            <StyledLink to="/sign-in">
+              <ButtonLogo>
+                <span>Sign In</span>
+              </ButtonLogo>
+            </StyledLink>
+            <StyledLink to="/sign-up">
+              <ButtonLogo>
+                <span>Sign Up</span>
+              </ButtonLogo>
+            </StyledLink>
+          </Fragment>
+        )}
+      </StyledToolbar>
+    </StyledAppBar>
   </>
 );
 
@@ -95,7 +91,6 @@ const mapDispatchToProps = {
 };
 
 export default compose(
-  withTheme,
   connect(
     mapStateToProps,
     mapDispatchToProps,
