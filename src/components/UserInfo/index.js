36d43a4aca179d39a9ef228845da22ad6da70cb2
Bodyan322 <bogdan3206@gmail.com>
2019-04-10 16:39:15 +0300

    styledComponent/AdminPage, add styled components to UserInfo component

--- src/components/UserInfo/index.js
+++ src/components/UserInfo/index.js
@@ -1,29 +1,33 @@
 import React from 'react';
 import { userType } from 'types';
-
-import withStyles from '@material-ui/core/styles/withStyles';
 import ListItem from '@material-ui/core/ListItem';
 import Divider from '@material-ui/core/Divider';
-import Paper from '@material-ui/core/Paper';
-import Avatar from '@material-ui/core/Avatar';
 import ListItemText from '@material-ui/core/ListItemText';
-import Button from '@material-ui/core/Button';
 
-import styles from './style';
+import {
+  PaperWrapper,
+  HeaderWrapper,
+  BigAvatar,
+  Header,
+  InfoBodyWrapper,
+  ButtonWrapper,
+  StyledButton,
+  UserInfoWrapper,
+} from './style';
 
-const UserInfo = ({ selectedUser, classes, openModal }) => {
+const UserInfo = ({ selectedUser, openModal }) => {
   const { firstName, lastName, ava, email, role, phone } = selectedUser;
 
   return (
-    <Paper className={classes.paperWrapper}>
-      <div className={classes.headerWrapper}>
-        <Avatar src={ava} alt="avatar" className={classes.bigAvatar} />
-        <h2 className={classes.header}>
+    <PaperWrapper>
+      <HeaderWrapper>
+        <BigAvatar src={ava} alt="avatar" />
+        <Header>
           {firstName && lastName ? `${firstName} ${lastName}` : 'Unknown User'}
-        </h2>
-      </div>
-      <div className={classes.infoBodyWrapper}>
-        <div className={classes.userInfoWrapper}>
+        </Header>
+      </HeaderWrapper>
+      <InfoBodyWrapper>
+        <UserInfoWrapper>
           <ListItem>
             <ListItemText secondary={`Email: ${email || ' - '}`} />
           </ListItem>
@@ -35,39 +39,23 @@ const UserInfo = ({ selectedUser, classes, openModal }) => {
           <ListItem>
             <ListItemText secondary={`Role: ${role || 'user'}`} />
           </ListItem>
-        </div>
-        <div className={classes.buttonWrapper}>
-          <Button
-            className={classes.button}
-            variant="contained"
-            color="primary"
-            onClick={openModal}>
+        </UserInfoWrapper>
+        <ButtonWrapper>
+          <StyledButton variant="contained" color="primary" onClick={openModal}>
             Reset password
-          </Button>
-          <Button
-            className={classes.button}
-            variant="contained"
-            color="primary"
-            onClick={openModal}>
+          </StyledButton>
+          <StyledButton variant="contained" color="primary" onClick={openModal}>
             Delete user
-          </Button>
-          <Button
-            className={classes.button}
-            variant="contained"
-            color="primary"
-            onClick={openModal}>
+          </StyledButton>
+          <StyledButton variant="contained" color="primary" onClick={openModal}>
             Change role
-          </Button>
-          <Button
-            className={classes.button}
-            variant="contained"
-            color="primary"
-            onClick={openModal}>
+          </StyledButton>
+          <StyledButton variant="contained" color="primary" onClick={openModal}>
             User activity
-          </Button>
-        </div>
-      </div>
-    </Paper>
+          </StyledButton>
+        </ButtonWrapper>
+      </InfoBodyWrapper>
+    </PaperWrapper>
   );
 };
 
@@ -75,4 +63,4 @@ UserInfo.propTypes = {
   selectedUser: userType.isRequired,
 };
 
-export default withStyles(styles)(UserInfo);
+export default UserInfo;
--- src/components/UserInfo/style.js
+++ src/components/UserInfo/style.js
@@ -1,43 +1,49 @@
-const styles = theme => ({
-  paperWrapper: {
-    color: theme.palette.text.secondary,
-    padding: '0px 10px',
-    display: 'flex',
-    flexDirection: 'column',
-    width: '620px',
-  },
-  headerWrapper: {
-    width: '100%',
-    display: 'flex',
-    alignItems: 'center',
-    paddingTop: 20,
-    marginBottom: 20,
-  },
-  header: {
-    width: '100%',
-    textAlign: 'center',
-  },
-  infoBodyWrapper: {
-    display: 'flex',
-    justifyContent: 'space-between',
-    marginBottom: 20,
-  },
-  userInfoWrapper: {
-    width: '60%',
-  },
-  bigAvatar: {
-    marginLeft: 50,
-    width: '120px',
-    height: '120px',
-  },
-  buttonWrapper: {
-    display: 'flex',
-    flexDirection: 'column',
-    justifyContent: 'space-between',
-  },
-  button: {
-    margin: '0 5px 5px 5px',
-  },
-});
+import Paper from '@material-ui/core/Paper';
+import Avatar from '@material-ui/core/Avatar';
+import Button from '@material-ui/core/Button';
+import styled from 'styled-components';
 
-export default styles;
+export const PaperWrapper = styled(Paper)`
+  color: #fff;
+  padding: 0px 10px;
+  display: flex;
+  flex-direction: column;
+  width: 620px;
+`;
+export const HeaderWrapper = styled.div`
+  width: 100%;
+  display: flex;
+  align-items: center;
+  padding-top: 20px;
+  margin-bottom: 20px;
+`;
+
+export const BigAvatar = styled(Avatar)`
+  margin-left: 50px;
+  width: 120px;
+  height: 120px;
+`;
+export const Header = styled.h2`
+  width: 100%;
+  text-align: center;
+`;
+
+export const InfoBodyWrapper = styled.div`
+  display: flex;
+  justify-content: space-between;
+  margin-bottom: 20px;
+`;
+
+export const UserInfoWrapper = styled.div`
+  width: 60%;
+`;
+
+export const ButtonWrapper = styled.div`
+  display: flex;
+  flex-direction: column;
+  justify-content: space-between;
+`;
+
+export const StyledButton = styled(Button)`
+  margin: 0 5px 5px 5px !important;
+`;
